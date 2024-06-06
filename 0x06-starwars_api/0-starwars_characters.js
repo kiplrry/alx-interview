a4a7089bc35169f1b89a71fc36b770a672803b12
kiplrry <kiplarry19@gmail.com>
2024-06-07 00:20:13 +0300

    all

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+
+const filmId = process.argv[2];
+
+async function fetchStarWarsCharacters(id) {
+  const filmEndpoint = 'https://swapi-api.hbtn.io/api/films/' + id;
+  try {
+    const filmResponse = await request(filmEndpoint);
+    const filmData = JSON.parse(filmResponse.body);
+    const characters = filmData.characters;
+
+    for (let i = 0; i < characters.length; i++) {
+      const characterEndpoint = characters[i];
+      try {
+        const characterResponse = await request(characterEndpoint);
+        const characterData = JSON.parse(characterResponse.body);
+        console.log(characterData.name);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchStarWarsCharacters(filmId);
